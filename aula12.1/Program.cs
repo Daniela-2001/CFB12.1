21278760eb959e2b83d6e5b2a1f9ed542edd291b
Daniela Duarte <danielatavaresd@gmail.com>
2024-01-20 15:53:47 +0000

    CFB12.1

---
+++ aula12.1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace aula12._1
+{
+    internal class Program
+    {
+        static void Main(){
+
+            int nota = 10;
+
+            string resultado = "Reprovado";
+
+            if(nota >= 60){
+                resultado = "Aprovado";
+            }
+
+            Console.WriteLine("Resultado:{0}", resultado);
+        }
+    }
+}
